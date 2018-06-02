09a34102deb1ab5a720c99eddda73aa2b5bba612
Leon <Leon.ID@QQ.COM>
2018-06-02 22:47:54 +0800

    数据库连接有问题，可能和环境配置有关；

--- LeonNote/Controllers/AccountController.cs
+++ LeonNote/Controllers/AccountController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeonNote.Models;
 
 namespace LeonNote.Controllers
 {
     public class AccountController : Controller
     {
+        NoteDB noteDB = new NoteDB();
+
         public ActionResult LogOff()
         {
             Session.Clear();
@@ -16,6 +19,22 @@ namespace LeonNote.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var item = noteDB.UserBase.FirstOrDefault<User>(m => m.UserName == user.UserName && m.Password == user.Password);
+            if (item != null)
+            {
+                Session["User"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("","登录错误");
+                return View(user);
+            }
         }
 
         public ActionResult Register()
