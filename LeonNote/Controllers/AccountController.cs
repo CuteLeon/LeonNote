c102516696582a6ff1040ad54aa934f3a56cffe1
Leon <Leon.ID@QQ.COM>
2018-06-02 21:23:01 +0800

    增加Account控制器；登录页；修改搜索表单方法；

---
+++ LeonNote/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeonNote.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult LogOff()
+        {
+            Session.Clear();
+            return RedirectToAction("Login","Account");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
