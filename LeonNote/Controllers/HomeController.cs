530902b4891eca2b0aa07099e1b3ca784ea68a52
Leon <Leon.ID@QQ.COM>
2018-06-03 20:05:16 +0800

    Home/Index控制器增加显示笔记；

--- LeonNote/Controllers/HomeController.cs
+++ LeonNote/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeonNote.Models;
 
 namespace LeonNote.Controllers
 {
     public class HomeController : Controller
     {
+        NoteDB noteDB = new NoteDB();
+        
         public ActionResult Index()
         {
-            return View();
+            //TODO: 自动登录调试代码；
+            Session["User"] = noteDB.UserBase.First(u => u.UserName == "Leon");
+
+            if (Session["User"] == null) return View();
+            int UserID = (Session["User"] as User).Id;
+            var notes = noteDB.NoteBase
+                .Where(note => note.UserID == UserID)
+                .ToList();
+            return View(notes);
         }
 
         public ActionResult About()
