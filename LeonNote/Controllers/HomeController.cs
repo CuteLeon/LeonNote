d7d6d8b65c063277c1dafbd2b590cc3a712a445f
Leon <xiaoyankeji@hotmail.com>
2018-06-04 13:31:11 +0800

    增加发表和编辑功能；增加用户验证；

--- LeonNote/Controllers/HomeController.cs
+++ LeonNote/Controllers/HomeController.cs
@@ -10,11 +10,11 @@ namespace LeonNote.Controllers
     public class HomeController : Controller
     {
         NoteDB noteDB = new NoteDB();
-        
+
         public ActionResult Index()
         {
             //TODO: 自动登录调试代码；
-            //Session["User"] = noteDB.UserBase.First(u => u.UserName == "Leon");
+            Session["User"] = noteDB.UserBase.First(u => u.UserName == "Leon");
 
             if (Session["User"] == null) return View(new List<Note>());
             int UserID = (Session["User"] as User).Id;
@@ -40,14 +40,72 @@ namespace LeonNote.Controllers
 
         public ActionResult Remove()
         {
-            int id =Convert.ToInt32(Request.QueryString["id"]);
-            int userid = Convert.ToInt32(Request.QueryString["userid"]);
-            if (userid != (Session["User"] as User).Id)
+            if (Session["User"] == null)
+                return RedirectToAction("Login", "Account");
+            User user = Session["User"] as User;
+            int? id = Convert.ToInt32(Request.QueryString["id"]);
+            if (id == null)
             {
                 //弹窗同时后退一步
-                return Content("<script>alert('非法的用户请求');history.go(-1);</script>");
+                return Content("<script>alert('无效的用户请求');history.go(-1);</script>");
             }
-            noteDB.NoteBase.Remove(noteDB.NoteBase.FirstOrDefault(n => n.Id == id && n.UserID == userid));
+            noteDB.NoteBase.Remove(noteDB.NoteBase.FirstOrDefault(n => n.Id == id && n.UserID == user.Id));
+            noteDB.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Publish()
+        {
+            if (Session["User"] == null)
+                return RedirectToAction("Login", "Account");
+            else
+                return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Publish(Note note)
+        {
+            if (Session["User"] == null)
+                return RedirectToAction("Login", "Account");
+            User user = Session["User"] as User;
+
+            note.PublishTime = DateTime.Now;
+            note.UserID = user.Id;
+            noteDB.NoteBase.Add(note);
+            noteDB.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Edit()
+        {
+            if (Session["User"] == null)
+                return RedirectToAction("Login", "Account");
+            User user = Session["User"] as User;
+            int? id = Convert.ToInt32(Request.QueryString["id"]);
+            if (id == null)
+            {
+                //弹窗同时后退一步
+                return Content("<script>alert('无效的用户请求');history.go(-1);</script>");
+            }
+            var note = noteDB.NoteBase.FirstOrDefault(n => n.Id == id && n.UserID == user.Id);
+            return View(note);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Note note)
+        {
+            if (Session["User"] == null)
+                return RedirectToAction("Login", "Account");
+            User user = Session["User"] as User;
+            int? id = Convert.ToInt32(Request.QueryString["id"]);
+            if (id == null)
+            {
+                //弹窗同时后退一步
+                return Content("<script>alert('无效的用户请求');history.go(-1);</script>");
+            }
+            var ExistedNote = noteDB.NoteBase.FirstOrDefault(n => n.Id == id && n.UserID == user.Id);
+            ExistedNote.Title = note.Title;
+            ExistedNote.Content = note.Content;
             noteDB.SaveChanges();
             return RedirectToAction("Index", "Home");
         }
