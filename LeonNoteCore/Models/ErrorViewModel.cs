997415844f709464fa737e722d04b9a89176468e
Leon <xiaoyankeji@hotmail.com>
2018-06-23 23:27:03 +0800

    增加 .Net Core 2.1 项目，用于开发此网站的跨平台版本；

---
+++ LeonNoteCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeonNoteCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LeonNoteCore/Models/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeonNoteCore.Models
+{
+    public class Note
+    {
+
+        [DisplayName("笔记ID")]
+        public int Id { get; set; }
+
+        [DisplayName("标题"), Required]
+        public string Title { get; set; }
+
+        [DisplayName("内容"), Required]
+        public string Content { get; set; }
+
+        [DisplayName("发布时间"), Required, DataType(DataType.DateTime)]
+        public DateTime? PublishTime { get; set; }
+
+        [DisplayName("发布者ID")]
+        public int UserID { get; set; }
+
+        [DisplayName("用户")]
+        public virtual User User { get; set; }
+    }
+}
